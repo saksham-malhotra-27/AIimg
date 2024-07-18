61a40f88e8b71e032d8e8f536bba3fbb2d764f2e
Saksham <saksh.m.malhotra@gmail.com>
2024-07-18 18:17:05 +0530

    Landing Page

---
+++ src/components/Nav.tsx
@@ -0,0 +1,36 @@
+"use client"
+import React from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+interface NavProps {
+  isAuthenticated: boolean;
+}
+
+const Nav: React.FC= () => {
+  const isAuthenticated= false
+  return (
+    <nav className="sticky top-0 z-50 h-16  w-full flex flex-row justify-around items-center space-x-4 p-4 text-xl font-extralight bg-stone-950 bg-opacity-75 text-white">
+      <Link href="/explore">
+        <span className={`cursor-pointer hover:text-slate-200 `}>Explore</span>
+      </Link>
+      <Link href="/"  >
+        <span className={`cursor-pointer hover:text-slate-200`}>Home</span>
+      </Link>
+      <Link href="/generate" >
+        <span className={`cursor-pointer hover:text-slate-200 `}>Generate</span>
+      </Link>
+      {isAuthenticated ? (
+        <Link href="/profile" >
+          <span className={`cursor-pointer hover:text-slate-200`}>Profile</span>
+        </Link>
+      ) : (
+        <Link href="/login"  >
+          <span className={`cursor-pointer `}>Login</span>
+        </Link>
+      )}
+    </nav>
+  );
+};
+
+export default Nav;
