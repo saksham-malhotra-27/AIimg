5547fd7d8e84559e91b193b6eab3a19ef3cc83f0
Saksham <saksh.m.malhotra@gmail.com>
2024-07-21 13:12:03 +0530

    added loader

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+
+
+export default function Loading() {
+    return (
+        <div className="absolute inset-0 flex items-center justify-center bg-white bg-opacity-10 z-50">
+          <div className="loader"></div>
+        </div>
+    )
+  }
